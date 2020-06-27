f70f9c79aaf1dfcba5afebfdc3d7b311986bcf8f
mauricioolarte <reneolarte@yahoo.com>
2020-06-27 05:48:35 +0000

    working in console task 7, point 1

--- console.py
+++ console.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """Simple command processor example."""
     
     prompt = '(hbnb) '
-    # intro = '\n'
-    
-    # def do_prompt(self, line):
-    #     "Change the interactive prompt"
-    #     self.prompt = line + ': '
-
+        
     def do_EOF(self, line):
         """ implement exit command whit EOF"""
         return True
@@ -19,6 +16,35 @@ class HBNBCommand(cmd.Cmd):
         """ Quit command to exit the program """
         return True
 
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it (
+            to the JSON file) and prints the id. Ex: $ create BaseModel
+        """
+        if arg == '':
+            print('** class name missing **')
+        elif arg == 'BaseModel':
+            new_BaseModel = BaseModel()
+            new_BaseModel.save()
+            print('{}'.format(new_BaseModel.id))
+        else:
+            print('** class doesn\'t exist **')
+    
+    def do_show(self, *args):
+        """
+            Prints the string representation of an instance based on the class
+            name and id. Ex: $ show BaseModel 1234-1234-1234.
+        """
+        if len(args) < 1:
+            print('** class name missing **')
+        elif args[1] != 'BaseModel':
+            print('** class doesn\'t exist **')
+        if len(args) == 2 and args[1] == 'BaseModel':
+            print('** instance id missing **')
+        
+        
+        else:
+            print('** class doesn\'t exist **')
 
+    
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
