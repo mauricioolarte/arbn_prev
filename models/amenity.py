274caeccaeae99aff9b2d45d3f0c4974d00b2ee9
mauricioolarte <reneolarte@yahoo.com>
2020-06-28 00:53:35 +0000

    add classes place, review, amenety

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" this module content the class amenety
+    atributes:
+        name
+"""
+
+from models import storage
+from models.base_model import BaseModel
+
+
+
+class Amenity(BaseModel):
+    """ this class is amenity """
+
+        name = ""
---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" this module content the class amenety
+    atributes:
+        text, user_id, place_id
+"""
+
+from models import storage
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ this class is Review """
+
+    place_id = ""
+    user_id = ""
+    text = ""
