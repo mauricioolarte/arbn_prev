91b24b75ee02e79a59842be496973792339acb30
eddyszh <eddyszh@github.com>
2020-06-28 00:22:23 +0000

    Add update console and other classes

---
+++ models/city.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from base_model import BaseModel
+
+class City(BaseModel):
+    state_id = ""
+    name = ""
---
+++ models/user.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from model.model_base import ModelBase
+
+class User(ModelBase):
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
